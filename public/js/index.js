71b8669eb3c98806554ef7061e7d85e234e9a2a0
Wellington Salgado <wellingtonsalgadodeveloper@gmail.com>
2019-03-05 20:16:40 -0300

    desenvolvendo novo layout

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,8 @@
 $(document).ready(function(){
-
+  infoCards();
+  buscarProduto();
+  cards();
+  abreListaProdutos();
 });
 
 function loadProducts(){
@@ -13,4 +16,43 @@ function loadProducts(){
 
 function criarSecoes(){
   
+}
+//Função de Busca
+function buscarProduto(){
+  var btnBuscar = document.getElementById('btnBuscar');
+  btnBuscar.addEventListener('click', function(){
+    console.log('Pesquisar');
+  });
+}
+
+//Função de Informações de Seções
+function infoCards(){
+  var info = document.querySelector('.info');
+  info.addEventListener('touchstart',function(){
+    info.children[0].innerHTML = 'info';
+  });
+  info.addEventListener('touchend',function(){
+    info.children[0].innerHTML = 'info_outline';
+  });
+}
+
+//Função do Cards
+function cards(){
+  var cards = document.querySelectorAll('.cards');
+  cards.forEach(function(item, index){
+    item.addEventListener('touchstart', function(){
+      console.log(item.id);
+    });
+    item.addEventListener('touchend', function(){
+      
+    });
+  })
+}
+
+function abreListaProdutos(){
+  var irProdutos = document.querySelector('.irProdutos');
+  irProdutos.addEventListener('touchstart', function(){
+    var secao = irProdutos.parentNode.parentNode.parentNode.id;
+    alert('Abrir modal com lista de produtos da: '+ secao);
+  });
 }
