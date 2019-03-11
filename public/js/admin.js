a95b0b50a8406cd26b400c7ed2dabd7caed90c0f
Wellington Salgado <wellingtonsalgadodeveloper@gmail.com>
2019-03-11 12:29:50 -0300

    reversão para layout antigo

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,3 +1,22 @@
 $(document).ready(function(){
-    
+    addProduto();
+    fecharCadastro();
 });
+
+
+function addProduto(){
+    let btnAdd = document.querySelector('#floatAdd');
+    let containerCadastro = document.querySelector('#containerCadastro');
+    btnAdd.addEventListener('click',(e)=>{
+        containerCadastro.style.display = 'flex';
+    });
+}
+
+function fecharCadastro(){
+    let btnFechar = document.querySelector('#btnFechar');
+    let containerCadastro = document.querySelector('#containerCadastro');
+    console.log(btnFechar);
+    btnFechar.addEventListener('click',()=>{
+        containerCadastro.style.display = 'none';
+    });
+}
--- public/js/index.js
+++ public/js/index.js
@@ -1,12 +1,14 @@
+'use-strict';
 
 $(document).ready(function(){
-  
-  abreCategorias();
-  criarLista();
+  //buscarProduto();
+  loadMenus();
+
+
+
+
 
 });
-
-
 
 
 //Requisição GET
@@ -26,7 +28,7 @@ xhttp.onreadystatechange = function() {
        products = xhttp.responseText;
     }
 };
-xhttp.open("GET", "http://localhost:3000/products", false);
+xhttp.open("GET", "http://192.168.3.108:3000/products", false);
 xhttp.send();
 var produtos = JSON.parse(products);
 
@@ -104,60 +106,143 @@ var sucosVitamina = {
 ]
 }
 // Menus de Testes
-var menus = [cafeteria, sucosVitamina];
+//var menus = [cafeteria, sucosVitamina];
 
-//Função de abrir categorias
-function abreCategorias(){
-  var btnsCategorias = document.querySelectorAll('.abreLista');
-  var estadoCafeteria = false;
-  var estadoSucosVitaminas = false;
-  var cardsCategorias = Array.from(btnsCategorias);
-  cardsCategorias.forEach((e)=>{
-    
-    e.addEventListener('click',(e)=>{
 
-      if(e.target.parentNode.id == 'abreCafeteria'){
-        
-        if(estadoCafeteria == false){
-          e.target.innerHTML = 'keyboard_arrow_up'
-          estadoCafeteria = !estadoCafeteria
-        }else {
-          e.target.innerHTML = 'keyboard_arrow_down'
-          estadoCafeteria = !estadoCafeteria
-        }
-        
 
-      }
-      if(e.target.parentNode.id == 'abreSucosVitaminas'){
-        if(estadoSucosVitaminas == false){
-          e.target.innerHTML = 'keyboard_arrow_up'
-          estadoSucosVitaminas = !estadoSucosVitaminas
-        }else {
-          e.target.innerHTML = 'keyboard_arrow_down'
-          estadoSucosVitaminas = !estadoSucosVitaminas
-        }
-      }
-
+//Cria menus dinamicamente
+function loadMenus(){
+  if(produtos != undefined){
+    produtos.forEach(function(element,index){
+      criarMenu(element.secao,element.produtos,element.slug);
     });
+  }else{
+    alert('Sem produtos Cadastrados :(')
+  }
+  
+}
 
+function criarMenu(secaoName,secaoProdutos,idSecao){
+  //Recuperando referencia da div conteudo
+  let divConteudoLista = document.getElementById('conteudoLista');
+
+  //Criação do elemento menu
+  let card = document.createElement('div');
+  card.classList.add('card');
+
+  //Criação do icone do menu
+  var icone = document.createElement('i');
+  icone.classList.add('material-icons');
+  icone.innerHTML = 'local_cafe';
+
+  //Criação do nome do menu
+  var spanTitulo = document.createElement('span');
+  spanTitulo.innerHTML = secaoName;
+
+  //Criação de botão de abertura de lista
+  var spanAbreLista = document.createElement('span');
+  spanAbreLista.classList.add('abreLista');
+  spanAbreLista.id = idSecao;
+  var iconSpanAbreLista = document.createElement('i');
+  iconSpanAbreLista.classList.add('material-icons');
+  iconSpanAbreLista.innerHTML = 'keyboard_arrow_down';
+  spanAbreLista.append(iconSpanAbreLista);
+
+  //Adicionando nome, icones, e botão de abrir lista
+  card.append(icone);
+  card.append(spanTitulo);
+  card.append(spanAbreLista);
+
+  //Cria ul para listar produtos
+  var listaItens = document.createElement('ul');
+  listaItens.id = idSecao + '-list';
+
+  //Adicionando os menus ao conteudo da pagina
+  divConteudoLista.append(card);
+  divConteudoLista.append(listaItens);
+
+  //Metodo que cria a lista
+  abrirCard(spanAbreLista.id, listaItens.id, secaoProdutos)
+}
+
+function abrirCard(idBotao,idLista,produtos){
+  var stado = false;
+  let btnAbre = document.querySelector('#'+idBotao);
+  
+
+  var lista = document.querySelector('#'+idLista);
+  
+  
+
+  btnAbre.addEventListener('click',function(){
     
 
+    if(stado == false){
+      //Muda Icone
+      btnAbre.childNodes[0].innerHTML = 'keyboard_arrow_up';
+
+      //Cria lista de itens
+      produtos.forEach(function(elemento){
+        var item = document.createElement('li');
+        var valorItem = document.createElement('span');
+        valorItem.innerHTML = elemento.valor;
+        item.innerHTML = elemento.nome;
+        item.append(valorItem);
+        item.classList.add('produtos');
+        lista.append(item);
+        detalhesItem(item,produtos);
+      });
+      stado = !stado;
+    }else{
+      //Muda Icone Para Original
+      btnAbre.childNodes[0].innerHTML = 'keyboard_arrow_down';
+      var itens = Array.from(lista.childNodes);
+      itens.forEach(function(element){
+        lista.removeChild(element);
+      })
+     
+      stado = !stado;
+    }
+
   });
-  
-  
 }
 
-//Função que cria lista de itens
-function criarLista(){
-  var ul = document.createElement('ul');
-  produtos.forEach((element, index)=> {
-    // statements
-    console.log(element.produtos)
-  });
+function detalhesItem(obj,secao){
+  let stado = false;
+
+  obj.addEventListener('click', function(){
+    
+
+    if(stado == false){
+      
+      //cria elementos dinamicamente
+      let divDetalhes = document.createElement('div');
+      divDetalhes.classList.add('detalhesProdutos');
+      let desc = document.createElement('span');
+
+      //forEach para verificação de itens
+      let valorDesc = '';
+      let nomeObjeto = Array.from(obj.childNodes)
+      secao.forEach(function(item, index){
+        console.log(item.nome);
+        if(item.nome == nomeObjeto[0].data){
+          valorDesc = item.descricao;
+        }
+      });
+      console.log(valorDesc);
+      console.log(nomeObjeto[0])
+      
+      //Atribui valores
+      desc.innerHTML = valorDesc;
+      divDetalhes.append(desc);
+      obj.append(divDetalhes);
+      obj.classList.add('detalhes');
+      stado = !stado;
+    }else{
+      obj.classList.remove('detalhes');
+      obj.removeChild(obj.childNodes[2])
+      stado = !stado;
+    }
+    
+  })
 }
-
-
-
-
-
-
