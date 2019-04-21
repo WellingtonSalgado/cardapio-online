1bdeb3c9ce8868f063b19df729512c3f2b1fd13f
Macbook <macbook@MacBook-de-MacBook-2.local>
2019-04-21 11:05:53 -0300

    refatoração de codigo

--- public/js/eventos.js
+++ public/js/eventos.js
@@ -1,6 +1,8 @@
-$(document).ready(function(){
+'use-strict';
+
+window.onload = function(){
     mostraEventos();
-});
+}
 
 
 function mostraEventos(){
--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,57 @@
 'use-strict';
 
-$(document).ready(function(){
-  //buscarProduto();
+/*$(document).ready(function(){
+  
   loadMenus();
-  pesquisar();
+  
+
+
+  buscarArrayProdutos();
+
+
+});*/
+
+window.onload = function(){
+  loadMenus();
+  buscarArrayProdutos();
+}
+
+var produtos = []
+
+function buscarArrayProdutos(produtos){
+  var arrayProdutos = [];
+  for(let categ in produtos[0]){
+    for(let key in produtos[0][categ]){
+      
+      produtos.push(produtos[0][categ][key].nome);  
+      
+    }
+  }
+
+  produtos.forEach(function(produt, index){
+    if (index != 0) {
+      arrayProdutos.push(produt);
+    }
+  })
+  //Função de pesquisa
+  pesquisar(arrayProdutos);
+}
 
 
 
+function pesquisar(produtos){
 
-});
-
-
-
-function pesquisar(){
   var arrayProducts = produtos;
-  //var arrayPesquisa = ['café P','tapioca nordestina','tapioca de manteiga','suco de cajú', 'café G', 'suco de manga']
+  
   var arrayPesquisa = [];
   
   //Adiciona ao array de pesquisa
-  arrayProducts.forEach((e,i)=>{
-    e.produtos.forEach((p)=>{
-      arrayPesquisa.push(p.nome);
-    })
+  arrayProducts.forEach((e)=>{
+    
+    
+    arrayPesquisa.push(e);
+    
   });
-  
   
   let btnPesquisa = document.getElementById('iconeMenu');
   let state = false;
@@ -31,17 +59,14 @@ function pesquisar(){
   var secao = '';
   //Span que abre lista (Aqui deve ser colocado a secao)
   var spansAbreLista = document.querySelectorAll('.abreLista');
+  //console.log(spanAbreLista);
   var input = document.createElement('input');
   var div = document.createElement('div');
   btnPesquisa.addEventListener('click', ()=>{
     //Verifica estado do botao de pesquisa
     if(state == false){
       //Verifica se a seção ja esta aberta e fecha a mesma
-      spansAbreLista.forEach((s)=>{
-        if(s.openState){
-          s.click();
-        }
-      })
+      fecharCards();
       
       btnPesquisa.classList.add('pesquisar');
       input.type = 'search';
@@ -58,14 +83,15 @@ function pesquisar(){
 
   //Evento do Input
   input.addEventListener('input', (e)=>{
-    
-    div.childNodes.forEach((e)=>{
-      div.removeChild(e);
-    })
+
+    div.innerHTML = '';
     
     produtoPesquisado = input.value.trim();
     //Ordena os produtos
     arrayPesquisa = arrayPesquisa.sort();
+
+    let arrayProd = new Set([]);
+
     arrayPesquisa.forEach(function(item){
       if(input.value.trim() != '' ){
         //Verifica se contem produtos que correspondem a pesquisa
@@ -77,65 +103,50 @@ function pesquisar(){
           //Criar eventos de click e chama funçao de busca
           itens.addEventListener('click',(e)=>{
             var productSearch = e.target.innerHTML
+            
+            for(let cat in produtosFetch[0]){
+
+              //console.log(secao);
+
+              for(let prodt in produtosFetch[0][cat]){
+                
+
+                if (productSearch == produtosFetch[0][cat][prodt].nome) {
+                  secao = (cat.toLowerCase()).replace(/ /g, '-');
+
+                }
+              }
+
+            }
+
             //Verificar a secao do produto
             arrayProducts.forEach(e => {
-              e.produtos.forEach(p =>{
-                if(p.nome == productSearch){
+                
+                if(e == productSearch){
                   
-                  secao = e.slug
+                  //secao = e.toLowerCase();
+                  //console.log(secao);
                 }
-              })
+              
             })
             productSearch = productSearch.toUpperCase();
             buscarProduto(productSearch, secao);
           })
-
-
-          itens.innerHTML = item;
-          //Adiciona os itens
-          div.appendChild(itens);
-          
-          //Verifica a presença de itens repetidos
-          var list = [];
-          div.childNodes.forEach((elemento,index)=>{
-            if(elemento.innerHTML.includes(itens.innerHTML)){
-              
-              list.push(elemento.innerHTML);
-              list.forEach(()=>{
-
-                if(list.length > 1){
-                  //Verifica a exitencia do item
-                  if(div.contains(elemento)){
-                    div.removeChild(elemento);
-                  }
-                  
-                }
-              })
-            }
+           
             
-             
-          })
-          
-          
-          
+            arrayProd.add(item)
+
+            arrayProd.forEach(function(i){
+              itens.innerHTML = i;
+              div.appendChild(itens);
+              
+            })
+            
         }
         
       }
       
     })
-
-    //Limpa toda a pesquisa e suas variaveis
-    if(e.inputType == 'deleteContentBackward'){
-      input.value = '';
-      produtoPesquisado = '';
-      div.childNodes.forEach((i)=>{
-        div.removeChild(i);
-      })
-      if(div.childNodes.length >= 1){
-        div.removeChild(div.firstChild)
-      }
-      
-    }
     
   });
 
@@ -145,10 +156,7 @@ function pesquisar(){
       input.parentNode.removeChild(input);
       btnPesquisa.classList.remove('pesquisar');
       state = false;
-      div.childNodes.forEach((i)=>{
-      
-        div.removeChild(i);
-      })
+
       if(div.childNodes.length >= 1){
         div.removeChild(div.firstChild)
       }
@@ -158,32 +166,35 @@ function pesquisar(){
   })
 }
 
-//Requisição GET
 
-//$.get('http://localhost:3000/products',function(data, status){
-    //console.log(data);
-    //return data;
-//});
+//Request via fetch
+
+var produtosFetch = [];
+
+var fetchProdutos = fetch('https://delicias-app-db.firebaseio.com/produtos.json');
+fetchProdutos
+.then(function(response){
   
+  return response.json()
+})
+.then(function(myJson){
+  
+  produtosFetch.push(myJson);
 
-//Javascript puro GET
-var products = '';
-var xhttp = new XMLHttpRequest();
-xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-       // Typical action to be performed when the document is ready:
-       products = xhttp.responseText;
-    }
-};
-xhttp.open("GET", "http://192.168.1.5:3000/products", false);
-xhttp.send();
-var produtos = JSON.parse(products);
-
+  criarMenuFetch(produtosFetch);
+  buscarArrayProdutos(produtosFetch);
+  //getProdutosFetch(produtosFetch);
+  
+})
+.catch(function(error){
+  console.log(error.message)
+})
 
 
 
 //Função de Busca
 function buscarProduto(produtoPesquisado, secaoPesquisada){
+  
   var cardSecao = document.getElementById(secaoPesquisada);
   var listSecao = document.getElementById(`${secaoPesquisada}-list`);
   
@@ -194,127 +205,154 @@ function buscarProduto(produtoPesquisado, secaoPesquisada){
   //Abre o card
   setTimeout(()=>{
     cardSecao.click()
+    //Busca elemento pesquisado
+    for(let key in listSecao.childNodes){
+      var itemContent = listSecao.childNodes[key].innerHTML;
+      if(itemContent != undefined){
+        var nomeItem = itemContent.substring(0, itemContent.indexOf('<'));  
+      }
+      
+      if (nomeItem.toUpperCase() == produtoPesquisado) {
+        listSecao.childNodes[key].click();
+      }
+    }
 
-    //Busca o elemento pesquisado
-    listSecao.childNodes.forEach((elemt)=>{
-      elemt.childNodes.forEach((e)=>{
-        //Adiciona num array
-        var produtoText = e.textContent.split('\n');
-
-        if(produtoText[0].toUpperCase() === produtoPesquisado){
-          //Abre os detalhes do item
-          elemt.click();
-        }
-
-
-      })
-    })
   },400);
-  
-  
 
 }
 
+//Função que fecha todos os cards
+
+function fecharCards(){
+  if (window.scrollY <= 70) {
+    
+    let cardsAbertos = document.querySelectorAll('.abreLista');
+
+    for(let key in cardsAbertos){
+      if (!isNaN(key)) {
+        if (cardsAbertos[key].childNodes[0].innerHTML == 'keyboard_arrow_up') {
+          cardsAbertos[key].click();
+        }
+      }
+    }
+  }
+}
 
 //Cria menus dinamicamente
 function loadMenus(){
-  if(produtos != undefined){
-    produtos.forEach(function(element){
-      criarMenu(element.secao,element.produtos,element.slug);
-    });
-  }else{
-    alert('Ouve um erro no servidor :(')
-  }
+  pesquisar();
+  criarMenuFetch(produtosFetch);
   
 }
 
-function criarMenu(secaoName,secaoProdutos,idSecao){
-  //Recuperando referencia da div conteudo
-  let divConteudoLista = document.getElementById('conteudoLista');
+//Cria menus via fetch
 
-  //Criação do elemento menu
-  let card = document.createElement('div');
-  card.classList.add('card');
-
-  //Criação do icone do menu
-  var icone = document.createElement('i');
-  icone.classList.add('material-icons');
-  switch(secaoName){
-    case "Cafeteria":
-      icone.innerHTML = 'local_cafe';
-      break;
-    case "Sucos e Vitaminas":
-      icone.innerHTML = 'local_bar';
-      break;
-    case "Bebidas":
-      icone.innerHTML = 'local_bar';
-      break;
-    case "Sanduiches":
-      icone.innerHTML = 'local_pizza';
-      break;
-    case "Beirutes":
-      icone.innerHTML = 'local_pizza';
-      break;
-    case "Sobremesas":
-      icone.innerHTML = 'cake';
-      break;
-    case "Petiscos":
-      icone.innerHTML = 'bubble_chart';
-      break;
-    case "Self Service":
-      icone.innerHTML = 'restaurant_menu';
-      break;
-    case "A la Carte":
-      icone.innerHTML = 'room_service';
-      break;
-    default:
-      icone.innerHTML = 'restaurant';
-  }
-  
-  
+function criarMenuFetch(produtos){
   
 
-  //Criação do nome do menu
-  var spanTitulo = document.createElement('span');
-  spanTitulo.innerHTML = secaoName;
+  produtos.forEach( function(element) {
+    // statements
+    
 
-  //Criação de botão de abertura de lista
-  var spanAbreLista = document.createElement('span');
-  spanAbreLista.classList.add('abreLista');
-  spanAbreLista.id = idSecao;
-  spanAbreLista.openState = false;
-  var iconSpanAbreLista = document.createElement('i');
-  iconSpanAbreLista.classList.add('material-icons');
-  iconSpanAbreLista.innerHTML = 'keyboard_arrow_down';
-  spanAbreLista.append(iconSpanAbreLista);
+    for(let key in element){
 
-  //Adicionando nome, icones, e botão de abrir lista
-  card.append(icone);
-  card.append(spanTitulo);
-  card.append(spanAbreLista);
+        //Recuperando referencia da div conteudo
+        let divConteudoLista = document.getElementById('conteudoLista');
 
-  //Cria ul para listar produtos
-  var listaItens = document.createElement('ul');
-  listaItens.id = idSecao + '-list';
+        //Criação do elemento menu
+        let card = document.createElement('div');
+        card.classList.add('card');
 
-  //Adicionando os menus ao conteudo da pagina
-  divConteudoLista.append(card);
-  divConteudoLista.append(listaItens);
+        //Criação do icone do menu
+        var icone = document.createElement('i');
+        icone.classList.add('material-icons');
+        switch(key){
+          case "Cafeteria":
+            icone.innerHTML = 'local_cafe';
+            break;
+          case "Sucos e Vitaminas":
+            icone.innerHTML = 'local_bar';
+            break;
+          case "Bebidas":
+            icone.innerHTML = 'local_bar';
+            break;
+          case "Sanduiches":
+            icone.innerHTML = 'local_pizza';
+            break;
+          case "Beirutes":
+            icone.innerHTML = 'local_pizza';
+            break;
+          case "Sobremesas":
+            icone.innerHTML = 'cake';
+            break;
+          case "Petiscos":
+            icone.innerHTML = 'bubble_chart';
+            break;
+          case "Self Service":
+            icone.innerHTML = 'restaurant_menu';
+            break;
+          case "A la Carte":
+            icone.innerHTML = 'room_service';
+            break;
+          default:
+            icone.innerHTML = 'restaurant';
+        }
 
-  //Metodo que cria a lista
-  abrirCard(spanAbreLista.id, listaItens.id, secaoProdutos)
+        //Criação do nome do menu
+        var spanTitulo = document.createElement('span');
+        spanTitulo.innerHTML = key;
+
+        //Criação de botão de abertura de lista
+        var spanAbreLista = document.createElement('span');
+        spanAbreLista.classList.add('abreLista');
+        spanAbreLista.id = key.toLowerCase().replace(/ /g,'-');
+        spanAbreLista.openState = false;
+        var iconSpanAbreLista = document.createElement('i');
+        iconSpanAbreLista.classList.add('material-icons');
+        iconSpanAbreLista.innerHTML = 'keyboard_arrow_down';
+        spanAbreLista.appendChild(iconSpanAbreLista);
+
+        //Adicionando nome, icones, e botão de abrir lista
+        card.appendChild(icone);
+        card.appendChild(spanTitulo);
+        card.appendChild(spanAbreLista);
+
+        //Cria ul para listar produtos
+        var listaItens = document.createElement('ul');
+        listaItens.id = spanAbreLista.id + '-list';
+
+        //Adicionando os menus ao conteudo da pagina
+        divConteudoLista.appendChild(card);
+        divConteudoLista.appendChild(listaItens);
+
+        
+        
+        //Metodo que cria a lista
+        abrirCardFetch(spanAbreLista.id, listaItens.id, produtos);
+
+    }
+
+  });
+  
 }
 
-function abrirCard(idBotao,idLista,produtos){
+
+
+function abrirCardFetch(idBotao,idLista, produtos){
   var stado = false;
+
   let btnAbre = document.querySelector('#'+idBotao);
   
 
   var lista = document.querySelector('#'+idLista);
-  
-  
 
-  btnAbre.addEventListener('click',function(){
+  
+  //Array de Categorias
+  var arrayCateg = Object.keys(produtos[0]);
+
+
+  btnAbre.addEventListener('click',function(e){
+
 
     //estado do botao 
     btnAbre.openState = !btnAbre.openState;
@@ -323,33 +361,58 @@ function abrirCard(idBotao,idLista,produtos){
       //Muda Icone
       btnAbre.childNodes[0].innerHTML = 'keyboard_arrow_up';
 
-      //Cria lista de itens
-      produtos.forEach(function(elemento){
-        var item = document.createElement('li');
-        var valorItem = document.createElement('span');
-        valorItem.innerHTML = elemento.valor;
-        item.innerHTML = elemento.nome;
-        item.append(valorItem);
-        item.classList.add('produtos');
-        lista.append(item);
-        detalhesItem(item,produtos);
-      });
+        arrayCateg.forEach( function(categ) {
+          // statements
+
+          var arrayId = Object.keys(produtos[0][categ]);
+
+          arrayId.forEach( function(id) {
+            // statements
+
+            if (idBotao == categ.toLowerCase().replace(/ /g, '-')) {
+              
+
+              var produto = produtos[0][categ][id];
+
+
+              var item = document.createElement('li');
+              var valorItem = document.createElement('span');
+              valorItem.innerHTML = produto.valor;
+              item.innerHTML = produto.nome;
+              item.appendChild(valorItem);
+              item.classList.add('produtos');
+              lista.appendChild(item);
+
+              //Eventos de detalhes
+              detalhesItem(item, produto);
+
+            }  
+
+
+          });
+
+
+        });
+
+      
       stado = !stado;
     }else{
       //Muda Icone Para Original
-      btnAbre.childNodes[0].innerHTML = 'keyboard_arrow_down';
+
       var itens = Array.from(lista.childNodes);
       itens.forEach(function(element){
         lista.removeChild(element);
       })
-     
+
+      btnAbre.childNodes[0].innerHTML = 'keyboard_arrow_down';
       stado = !stado;
     }
 
   });
 }
 
-function detalhesItem(obj,secao){
+//Função de detalhes dos produtos
+function detalhesItem(obj,produto){
   let stado = false;
 
   obj.addEventListener('click', function(){
@@ -365,18 +428,20 @@ function detalhesItem(obj,secao){
       //forEach para verificação de itens
       let valorDesc = '';
       let nomeObjeto = Array.from(obj.childNodes)
-      secao.forEach(function(item, index){
+      
         
-        if(item.nome == nomeObjeto[0].data){
-          valorDesc = item.descricao;
-        }
-      });
+      if(produto.nome == nomeObjeto[0].data){
+        valorDesc = produto.descricao;
+      }
+      
       
       
       //Atribui valores
       desc.innerHTML = valorDesc;
-      divDetalhes.append(desc);
-      obj.append(divDetalhes);
+      desc.style.marginTop = '10px';
+      desc.style.paddingBottom = '0px';
+      divDetalhes.appendChild(desc);
+      obj.appendChild(divDetalhes);
       obj.classList.add('detalhes');
       stado = !stado;
     }else{
